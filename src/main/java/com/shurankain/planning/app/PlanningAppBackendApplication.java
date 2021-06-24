60f3e0699b767237e69d2135ab6714f7803642dc
shurankain <shurankain@gmail.com>
2021-06-24 23:18:56 +0200

    initial commit

---
+++ src/main/java/com/shurankain/planning/app/PlanningAppBackendApplication.java
@@ -0,0 +1,12 @@
+package com.shurankain.planning.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanningAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlanningAppBackendApplication.class, args);
+    }
+}
---
+++ src/test/java/com/shurankain/planning/app/PlanningAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shurankain.planning.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanningAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
