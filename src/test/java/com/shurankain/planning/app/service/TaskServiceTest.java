7d3cf5262d0e5a31c8641a0123ac04d802317377
shurankain <shurankain@gmail.com>
2021-06-26 12:59:20 +0200

    captors usage added

--- src/test/java/com/shurankain/planning/app/service/TaskServiceTest.java
+++ src/test/java/com/shurankain/planning/app/service/TaskServiceTest.java
@@ -2,6 +2,8 @@ package com.shurankain.planning.app.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -12,6 +14,8 @@ import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -27,6 +31,8 @@ class TaskServiceTest {
     public static final String TEST_TASK_INFO = "testTaskInfo";
     public static final LocalDateTime TEST_TASK_DATE = LocalDateTime.of(2021, 6, 26, 11, 20);
 
+    @Captor
+    private ArgumentCaptor<Task> taskCaptor;
     @Mock
     private TaskRepository taskRepository;
     @InjectMocks
@@ -38,26 +44,38 @@ class TaskServiceTest {
 
         var addedTask = taskService.addTask(TEST_TASK_INFO);
 
-        verify(taskRepository, times(1)).insert(any(Task.class));
+        verify(taskRepository, times(1)).insert(taskCaptor.capture());
         assertEquals(TEST_TASK_ID, addedTask.getId());
         assertEquals(TEST_TASK_INFO, addedTask.getTaskInfo());
         assertEquals(TEST_TASK_DATE, addedTask.getCreationDate());
         assertFalse(addedTask.isCompletionStatus());
+
+        Task capturedTask = taskCaptor.getValue();
+        assertNull(capturedTask.getId());
+        assertEquals(TEST_TASK_INFO, capturedTask.getTaskInfo());
+        assertNotNull(capturedTask.getCreationDate());
+        assertFalse(capturedTask.isCompletionStatus());
     }
 
     @Test
-    void whenEditTaskMethodInvokedWithProperDataThenTaskIdUpdated(){
+    void whenEditTaskMethodInvokedWithProperDataThenTaskIdUpdated() {
         when(taskRepository.findById(TEST_TASK_ID)).thenReturn(Optional.of(createMockTask()));
         when(taskRepository.save(any(Task.class))).thenReturn(createMockTask());
 
         var addedTask = taskService.editTask(createMockTaskDto());
 
         verify(taskRepository, times(1)).findById(TEST_TASK_ID);
-        verify(taskRepository, times(1)).save(any(Task.class));
+        verify(taskRepository, times(1)).save(taskCaptor.capture());
         assertEquals(TEST_TASK_ID, addedTask.getId());
         assertEquals(TEST_TASK_INFO, addedTask.getTaskInfo());
         assertEquals(TEST_TASK_DATE, addedTask.getCreationDate());
         assertFalse(addedTask.isCompletionStatus());
+
+        Task capturedTask = taskCaptor.getValue();
+        assertEquals(TEST_TASK_ID, capturedTask.getId());
+        assertEquals(TEST_TASK_INFO, capturedTask.getTaskInfo());
+        assertEquals(TEST_TASK_DATE, capturedTask.getCreationDate());
+        assertFalse(capturedTask.isCompletionStatus());
     }
 
     private Task createMockTask() {
